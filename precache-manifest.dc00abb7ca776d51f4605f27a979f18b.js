015612bd3e4afee81d311dc3a5da7df91af34533
Sayed Abdul <sakarim9124@gmail.com>
2019-04-20 15:50:10 +0530

    Updates

---
+++ precache-manifest.dc00abb7ca776d51f4605f27a979f18b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24b25aef5b9048a00409",
+    "url": "/ecom/static/js/runtime~main.a345f0af.js"
+  },
+  {
+    "revision": "67705f0e673e71bfa55c",
+    "url": "/ecom/static/js/main.96e8ce64.chunk.js"
+  },
+  {
+    "revision": "f718424254b17daa6ed6",
+    "url": "/ecom/static/js/2.8e3b7bbf.chunk.js"
+  },
+  {
+    "revision": "67705f0e673e71bfa55c",
+    "url": "/ecom/static/css/main.bdb189a8.chunk.css"
+  },
+  {
+    "revision": "f718424254b17daa6ed6",
+    "url": "/ecom/static/css/2.14e1d2de.chunk.css"
+  },
+  {
+    "revision": "4ec29ddcfad8ca11bfe1701391a7e2e9",
+    "url": "/ecom/index.html"
+  }
+];
